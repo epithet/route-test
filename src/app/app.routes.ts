209c63232c5c05ec4f6dc2bb16b73db4b6d567aa
Sébastien Hoffmann <sog@i.net>
2025-11-29 11:16:52 +0100

    save last viewed page per category

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,25 +7,31 @@ import { Cat2 } from './cat2/cat2';
 import { Foo } from './cat2/foo/foo';
 import { Bar } from './cat2/bar/bar';
 
+export const ROUTE_SAVE_KEY = 'SaveRoute';
+const ID_CAT1_PAGE = 'cat1_page';
+const ID_CAT2_PAGE = 'cat2_page';
+
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'cat1' },
   {
     path: 'cat1',
     component: Cat1,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'bla' },
-      { path: 'bla', component: Bla },
-      { path: 'bli', component: Bli },
-      { path: 'blu', component: Blu },
+      { path: '', pathMatch: 'full',
+        redirectTo: () => localStorage.getItem(ID_CAT1_PAGE) ?? 'bla' },
+      { path: 'bla', component: Bla, data: { [ROUTE_SAVE_KEY]: ID_CAT1_PAGE } },
+      { path: 'bli', component: Bli, data: { [ROUTE_SAVE_KEY]: ID_CAT1_PAGE } },
+      { path: 'blu', component: Blu, data: { [ROUTE_SAVE_KEY]: ID_CAT1_PAGE } },
     ],
   },
   {
     path: 'cat2',
     component: Cat2,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'foo' },
-      { path: 'foo', component: Foo },
-      { path: 'bar', component: Bar },
+      { path: '', pathMatch: 'full',
+        redirectTo: () => localStorage.getItem(ID_CAT2_PAGE) ?? 'foo' },
+      { path: 'foo', component: Foo, data: { [ROUTE_SAVE_KEY]: ID_CAT2_PAGE } },
+      { path: 'bar', component: Bar, data: { [ROUTE_SAVE_KEY]: ID_CAT2_PAGE } },
     ],
   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ActivationEnd, Event, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { ROUTE_SAVE_KEY } from './app.routes';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,18 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
 })
 export class App {
+  private readonly router = inject(Router);
+
+  constructor() {
+    this.router.events.pipe(takeUntilDestroyed()).subscribe((event: Event) => {
+      if (event instanceof ActivationEnd) {
+        const routeConfig = event.snapshot.routeConfig;
+        const saveKey = routeConfig?.data?.[ROUTE_SAVE_KEY];
+        const page = routeConfig?.path;
+        if (saveKey !== undefined && page !== undefined) {
+          localStorage.setItem(saveKey, page);
+        }
+      }
+    });
+  }
 }
