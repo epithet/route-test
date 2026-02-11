e9599a3d743773392188ae21718f2d48f35f322b
Sébastien Hoffmann <sog@i.net>
2026-02-11 11:21:03 +0100

    declare route save key only once in parent route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ActivatedRouteSnapshot, ActivationEnd, Event, ResolveFn, Router, Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, ActivationEnd, Event, RedirectFunction, ResolveFn, Router, Routes } from '@angular/router';
 import { Category } from './category/category';
 import { Bla } from './cat1/bla/bla';
 import { Bli } from './cat1/bli/bli';
@@ -9,20 +9,21 @@ import { Foo } from './cat2/foo/foo';
 import { Bar } from './cat2/bar/bar';
 
 const ROUTE_SAVE_KEY = 'SaveRoute';
-const ID_CAT1_PAGE = 'cat1_page';
-const ID_CAT2_PAGE = 'cat2_page';
 
 export function initRouting() {
   inject(Router).events.pipe(takeUntilDestroyed()).subscribe((event: Event) => {
     if (event instanceof ActivationEnd) {
-      const routeConfig = event.snapshot.routeConfig;
-      const saveKey = routeConfig?.data?.[ROUTE_SAVE_KEY];
-      const page = routeConfig?.path;
+      const saveKey = event.snapshot.pathFromRoot.at(-2)?.routeConfig?.data?.[ROUTE_SAVE_KEY];
+      const page = event.snapshot.routeConfig?.path;
       if (saveKey !== undefined && page !== undefined) {
         localStorage.setItem(saveKey, page);
       }
     }
   });
+}
+
+function createRedirectFunction(defaultRedirect: string): RedirectFunction {
+  return route => localStorage.getItem(route.data[ROUTE_SAVE_KEY]) ?? defaultRedirect;
 }
 
 const pagesResolver: ResolveFn<string[]> = (route: ActivatedRouteSnapshot) =>
@@ -34,23 +35,23 @@ export const routes: Routes = [
     path: 'cat1',
     component: Category,
     resolve: { pages: pagesResolver },
+    data: { [ROUTE_SAVE_KEY]: 'cat1_page' },
     children: [
-      { path: '', pathMatch: 'full',
-        redirectTo: () => localStorage.getItem(ID_CAT1_PAGE) ?? 'bla' },
-      { path: 'bla', component: Bla, data: { [ROUTE_SAVE_KEY]: ID_CAT1_PAGE } },
-      { path: 'bli', component: Bli, data: { [ROUTE_SAVE_KEY]: ID_CAT1_PAGE } },
-      { path: 'blu', component: Blu, data: { [ROUTE_SAVE_KEY]: ID_CAT1_PAGE } },
+      { path: '', pathMatch: 'full', redirectTo: createRedirectFunction('bla') },
+      { path: 'bla', component: Bla },
+      { path: 'bli', component: Bli },
+      { path: 'blu', component: Blu },
     ],
   },
   {
     path: 'cat2',
     component: Category,
     resolve: { pages: pagesResolver },
+    data: { [ROUTE_SAVE_KEY]: 'cat2_page' },
     children: [
-      { path: '', pathMatch: 'full',
-        redirectTo: () => localStorage.getItem(ID_CAT2_PAGE) ?? 'foo' },
-      { path: 'foo', component: Foo, data: { [ROUTE_SAVE_KEY]: ID_CAT2_PAGE } },
-      { path: 'bar', component: Bar, data: { [ROUTE_SAVE_KEY]: ID_CAT2_PAGE } },
+      { path: '', pathMatch: 'full', redirectTo: createRedirectFunction('foo') },
+      { path: 'foo', component: Foo },
+      { path: 'bar', component: Bar },
     ],
   },
 ];
